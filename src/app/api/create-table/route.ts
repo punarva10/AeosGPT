2bc3fe0cff28a559b38131155eeab4f52c7e2d70
Punarv Dinakar <pdinakar@gigsky.com>
2024-06-15 20:48:41 +0530

    postgrescheck

--- src/app/api/create-table/route.ts
+++ src/app/api/create-table/route.ts
@@ -3,8 +3,10 @@ import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
   try {
+    console.log("trying to create table");
     const result =
       await sql`CREATE TABLE New_table ( Name varchar(255), Owner varchar(255) );`;
+    console.log("created table");
     return NextResponse.json({ result }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
