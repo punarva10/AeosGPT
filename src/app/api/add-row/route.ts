043787917c372510a916fb5bf567e4acfa631dfa
Punarv Dinakar <pdinakar@gigsky.com>
2024-06-15 22:36:56 +0530

    postgres_working

--- src/app/api/add-row/route.ts
+++ src/app/api/add-row/route.ts
@@ -8,7 +8,7 @@ export async function GET(request: Request) {
 
   try {
     if (!petName || !ownerName) throw new Error("Pet and owner names required");
-    await sql`INSERT INTO final_test_table (Name, Owner) VALUES (${petName}, ${ownerName});`;
+    await sql`INSERT INTO pets (Name, Owner) VALUES (${petName}, ${ownerName});`;
     return NextResponse.json({}, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
