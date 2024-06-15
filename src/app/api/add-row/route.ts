ffe7cbf38d597d0da40615e08c358e32c09c5494
Punarv Dinakar <pdinakar@gigsky.com>
2024-06-15 21:55:40 +0530

    Hmm

--- src/app/api/add-row/route.ts
+++ src/app/api/add-row/route.ts
@@ -9,8 +9,7 @@ export async function GET(request: Request) {
   try {
     if (!petName || !ownerName) throw new Error("Pet and owner names required");
     await sql`INSERT INTO final_test_table (Name, Owner) VALUES (${petName}, ${ownerName});`;
-    // const pets = await sql`SELECT * FROM last_table;`;
-    // return NextResponse.json({ pets }, { status: 200 });
+    return NextResponse.json({}, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
   }
