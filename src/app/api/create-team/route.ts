d76b4b409f612b785aff2e9ca53679aa85fae02e
Punarv Dinakar <pdinakar@gigsky.com>
2024-06-16 22:12:43 +0530

    Working team creation

---
+++ src/app/api/create-team/route.ts
@@ -0,0 +1,56 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
+import db from "@/lib/db";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  if (request.method !== "POST") {
+    return NextResponse.json(
+      { message: "Method not allowed" },
+      { status: 405 }
+    );
+  }
+  const session = await getServerSession(authOptions);
+  if (!session?.user) {
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+  }
+
+  const body = await request.json();
+  const { teamName } = body;
+
+  if (!teamName || typeof teamName !== "string") {
+    return NextResponse.json({ message: "Invalid team name" }, { status: 400 });
+  }
+
+  const user = await db.users.findUnique({
+    where: {
+      email: session.user.email,
+    },
+  });
+
+  try {
+    const team = await db.teams.create({
+      data: {
+        name: teamName,
+        owner_id: user!.id,
+      },
+    });
+
+    await db.memberships.create({
+      data: {
+        team_id: team.id,
+        user_id: user!.id,
+      },
+    });
+
+    return NextResponse.json({ team }, { status: 201 });
+  } catch (error) {
+    console.error(error);
+
+    return NextResponse.json(
+      { message: "Internal server error" },
+      { status: 500 }
+    );
+  }
+}
