fd501a7f27a592478aaf7904177fcbedb6911366
Punarv Dinakar <pdinakar@gigsky.com>
2024-06-17 10:52:17 +0530

    Home page reroutes to auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+'use client'
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter()
+  router.push("/auth")
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Final check
-    </main>
+    <></>
   );
 }
