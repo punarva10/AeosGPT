b0970505a22a85e2edc30e88f094993e51d0241b
Punarv Dinakar <pdinakar@gigsky.com>
2024-06-18 20:15:07 +0530

    Clean up

--- src/app/api/check-credit/route.ts
+++ src/app/api/check-credit/route.ts
@@ -1,7 +1,6 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 import db from "@/lib/db";
+import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
 import nodemailer from "nodemailer";
 
